486314f77079cfe8405f063a3ed76136a4606f25
Gregory Singleton <yadig69design@gmail.com>
2020-06-25 03:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+var destructivelyAppendKitten = (val) => {
+  kittens.push(val)
+  return kittens
+}
+
+var destructivelyPrependKitten = (val) => {
+  kittens.unshift(val)
+  return kittens
+}
+
+var destructivelyRemoveLastKitten = (val) => {
+  kittens.pop(val)
+  return kittens
+}
+
+var destructivelyRemoveFirstKitten = (val) => {
+  kittens.shift(val)
+  return kittens
+}
+
+var appendKitten = (val) => {
+  var newArray = [...kittens]// duplacated the orginal array 
+  newArray.push(val)
+  return newArray
+}
+
+var prependKitten = (val) => {
+  var newArray = [...kittens]// duplacated the orginal array 
+  newArray.unshift(val)
+  return newArray
+}
+
+var appendKitten = (val) => {
+  var newArray = [...kittens]// duplacated the orginal array 
+  newArray.push(val)
+  return newArray
+}
+
+var prependKitten = (val) => {
+  var newArray = [...kittens]// duplacated the orginal array 
+  newArray.unshift(val)
+  return newArray
+}
